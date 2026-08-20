0ddc3ae110b05e1a1036e83167b056c837a0e01b
Thom Mott <thommott@proton.me>
2026-08-20 07:30:35 +0200

    C# Support update

--- modules/toolchains/csharp.nix
+++ modules/toolchains/csharp.nix
@@ -1,17 +1,13 @@
-{ pkgs, lib, ... }:
-{
+{ pkgs, ... }: {
   csharp = pkgs.buildEnv {
     name = "lang-csharp";
-    paths =
-      with pkgs;
-      [
-        dotnet-sdk_8
-        dotnet-runtime_8
-        dotnet-aspnetcore_8
-      ]
-      ++ lib.optionals pkgs.stdenv.isLinux [
-        omnisharp-roslyn
-        mono
-      ];
+    paths = with pkgs; [
+      dotnet-sdk_8
+      dotnet-runtime_8
+      dotnet-aspnetcore_8
+      msbuild
+      omnisharp-roslyn
+      mono
+    ];
   };
 }
